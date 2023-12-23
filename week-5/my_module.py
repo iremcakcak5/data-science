172af4cb5cd7ecd05ecde7a05e003a34ec8ea416
iremcakcak5 <iremcakcak1012@gmail.com>
2023-12-23 10:51:12 +0300

    week-5

---
+++ week-5/my_module.py
@@ -0,0 +1,10 @@
+def my_function():
+     print("Hello! ")
+
+#sadece dosya çalışırken çalışsın istenilenler yazılır
+ #import yapılınca çağtılmadan gelmemesi için, kontrol için if bloğu
+
+     
+if __name__ == "__main__":
+  my_function()
+  
---
+++ week-5/run.py
@@ -0,0 +1,2 @@
+from my_module import my_function
+
